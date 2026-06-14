6a2c01dec8163dd27b9024a57bda5ab4362b745e
Tsaiyu-0315 <alittlefish0315@gmail.com>
2026-06-14 22:35:56 +0800

    fix

--- 19/Circle.java
+++ 19/Circle.java
@@ -9,35 +9,20 @@ public class Circle extends Shape {
      * @param length 直徑
      */
     public Circle(double length) {
-        setLength(length);
+        super(length);
     }
 
-    /**
-     * 設定圓形的直徑。
-     *
-     * @param length 直徑
-     */
     @Override
     public void setLength(double length) {
         this.length = length;
     }
 
-    /**
-     * 計算圓形面積。
-     *
-     * @return 面積
-     */
     @Override
     public double getArea() {
         double radius = length / 2;
         return Math.PI * radius * radius;
     }
 
-    /**
-     * 計算圓形周長。
-     *
-     * @return 周長
-     */
     @Override
     public double getPerimeter() {
         return Math.PI * length;
