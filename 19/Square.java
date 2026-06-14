25d0a50c56cc241f34b9b998ed0beccfc779934b
Tsaiyu-0315 <alittlefish0315@gmail.com>
2026-06-14 22:33:32 +0800

    Fix shape constructors

--- 19/Square.java
+++ 19/Square.java
@@ -9,34 +9,19 @@ public class Square extends Shape {
      * @param length 邊長
      */
     public Square(double length) {
-        setLength(length);
+        super(length);
     }
 
-    /**
-     * 設定正方形的邊長。
-     *
-     * @param length 邊長
-     */
     @Override
     public void setLength(double length) {
         this.length = length;
     }
 
-    /**
-     * 計算正方形面積。
-     *
-     * @return 面積
-     */
     @Override
     public double getArea() {
         return length * length;
     }
 
-    /**
-     * 計算正方形周長。
-     *
-     * @return 周長
-     */
     @Override
     public double getPerimeter() {
         return 4 * length;
--- 19/Triangle.java
+++ 19/Triangle.java
@@ -9,34 +9,19 @@ public class Triangle extends Shape {
      * @param length 邊長
      */
     public Triangle(double length) {
-        setLength(length);
+        super(length);
     }
 
-    /**
-     * 設定正三角形的邊長。
-     *
-     * @param length 邊長
-     */
     @Override
     public void setLength(double length) {
         this.length = length;
     }
 
-    /**
-     * 計算正三角形面積。
-     *
-     * @return 面積
-     */
     @Override
     public double getArea() {
         return Math.sqrt(3) * length * length / 4;
     }
 
-    /**
-     * 計算正三角形周長。
-     *
-     * @return 周長
-     */
     @Override
     public double getPerimeter() {
         return 3 * length;
